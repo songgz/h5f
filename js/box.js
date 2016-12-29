7c32fa948997762815dac5d582d464d31346ebf5
sgzhe <6485785@qq.com>
2016-12-29 17:27:52 +0800

    modify box
    Signed-off-by: sgzhe <6485785@qq.com>

--- js/box.js
+++ js/box.js
@@ -3,26 +3,47 @@
 
         var defaults = {
             width   : 100,
-            height  : 100,            
+            height  : 100, 
+            cls: 'box', 
+            'border-style': '#ff0000',          
             autoEl:'div', //{tag:'div',name:'ss'}
-            hidden:false,
-            allowDomMove:true       
+            content: ''    
         };
      
-        var settings = $.extend( {}, defaults, options );
-     
-        return this.each(function() {
-            var container = this;
-            var el = $('<div></div>',{
+        var settings = $.extend({}, defaults, options );
+
+        var mark = "";
+        var tag = "";
+        if(typeof settings.autoEl === "object"){
+            tag = settings.autoEl.tag;
+            mark += '<'+ tag;
+            foreach(var attr in settings.autoEl){
+                if (settings.autoEl.hasPropject(attr)){
+                    mark += ' attr="' + settings.autoEl[attr] + '"'
+                }
+            }
+        }else{
+            tag = settings.autoEl;
+            mark += tag;
+        }
+        mark += '>';
+        mark += settings.content;        
+        mark += '</' + tag + '>';     
+         
+
+
+        var el = $(mark, {
                 css:{
                     'border-style': 'solid',
                     'border-color': '#ff0000',
                     'border-width': 'thin',
-                    width: settings['width']+'px',
+                    width: settings.width +'px',
                     height: settings['height'] + 'px'
                 }
-            });
-            
+            });  
+     
+        return this.each(function() {
+            var container = this;            
             el.appendTo(container);
         });
  
